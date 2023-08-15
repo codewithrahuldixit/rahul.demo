f51d94df6ff22ff48e5c1672ca2d17b679beab5a
rahul <posttorahuldixit@gmail.com>
2023-08-15 19:14:21 +0530

    Checking and creating war

--- src/main/java/com/example/rahul/demo/ControllerDemo.java
+++ src/main/java/com/example/rahul/demo/ControllerDemo.java
@@ -1,14 +1,12 @@
 package com.example.rahul.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerDemo {
 
 	@GetMapping("/hello")
-	@ResponseBody
 	public String hello() {
 		return "Hello World!!!!!!!!";
 	}
